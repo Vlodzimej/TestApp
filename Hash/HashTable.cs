2acbe13e220014a71e68aa581e9c50531906c89c
Амелькин Владимир <amelkin@vbankcenter.ru>
2021-02-25 21:13:16 +0300

    Added. Hash. Реализация хэш-таблицы.

---
+++ Hash/HashTable.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestApp.Hash
+{
+    public class HashTable
+    {
+        private Dictionary<int, List<Item>> items = null;
+
+        public IReadOnlyCollection<KeyValuePair<int, List<Item>>> Items => items?.ToList()?.AsReadOnly();
+
+        public HashTable()
+        {
+            items = new Dictionary<int, List<Item>>(_maxSize);
+        }
+
+        public void Insert(string key, string value)
+        {
+            var item = new Item(key, value);
+            var hash = GetHash(item.Key);
+
+            List<Item> hashTableItem = null;
+
+            if (items.ContainsKey(hash))
+            {
+                hashTableItem = items[hash];
+                if (hashTableItem.SingleOrDefault(i => i.Key == item.Key) != null)
+                {
+                    return null;
+                }
+
+                items[hash].Add(item);
+            }
+            else
+            {
+                hashTableItem = new List<Item>{ item };
+                items.Add(hash, hashTableItem);
+            }
+        }
+
+        public void Delete(string key)
+        {
+            var hash = GetHash(key);
+
+            if (items.ContainsKey(hash))
+            {
+                return;
+            }
+
+            var hashTableItem = items[hash];
+            var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+
+            if (item != null)
+            {
+                hashTableItem.Remove(item);
+            }
+        }
+
+        public string Search(string key)
+        {
+            var hash = GetHash(key);
+
+            if(!items.ContainsKey(hash))
+            {
+                return null;
+            }
+
+            var hashTableItem = items[hash];
+            if (hashTableItem != null)
+            {
+                var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+
+                if (item != null)
+                {
+                   return item.Value;
+                }
+            }
+            return null;
+        }
+
+        private int GetHash(string value)
+        {
+            return value.Length;
+        }
+    }
+}
---
+++ Hash/Item.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace TestApp.HashTable
+{
+    public class Item
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public Item(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
